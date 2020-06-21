ec285e58ff3bf9fe25f000f0e9daae948e24285a
Tony <tgoldin@euler.01.vpc>
2020-06-21 21:44:13 +0100

    Added src dir and git ignore file

---
+++ src/main/java/com/tpg/targos/products/TargosProductsMsApplication.java
@@ -0,0 +1,13 @@
+package com.tpg.targos.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargosProductsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargosProductsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpg/targos/products/TargosProductsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpg.targos.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargosProductsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
